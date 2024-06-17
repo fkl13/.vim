1e590347665c2b8637a92afdfc3b62b1a4103056
Fabian Klaiber <fklaiber@posteo.net>
2024-06-17 22:03:59 +0200

    Add markdown plugin

--- init.lua
+++ init.lua
@@ -529,6 +529,24 @@ require("lazy").setup({
             -- refer to the configuration section below
         },
     },
+    {
+        "plasticboy/vim-markdown",
+        ft = { "markdown" },
+        dependencies = {
+            "godlygeek/tabular",
+        },
+        config = function()
+            -- never ever fold!
+            vim.g.vim_markdown_folding_disabled = 1
+            -- support front-matter in .md files
+            vim.g.vim_markdown_frontmatter = 1
+            -- 'o' on a list item should insert at same level
+            vim.g.vim_markdown_new_list_item_indent = 0
+            -- don't add bullets when wrapping:
+            -- https://github.com/preservim/vim-markdown/issues/232
+            vim.g.vim_markdown_auto_insert_bullets = 0
+        end,
+    },
 })
 
 -- vim: ts=4 sts=4 sw=4 et
